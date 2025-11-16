41c2b363e59bd35c25381a801b08417de7dc557b
Felix Traxler <e12217071@student.tuwien.ac.at>
2025-11-16 22:52:14 +0000

    feat: add X-Restormer to run_all script

--- run_all.py
+++ run_all.py
@@ -10,13 +10,18 @@ projects = [
         "args": ["-i", "../input_128/", "-o", "../output_images/"],
         "cwd": BASE_DIR / "DarkIR",  # important: run inside the project folder
     },
+    {
+        "venv": BASE_DIR / "X-Restormer/venv/bin/python",
+        "script": BASE_DIR / "X-Restormer/xrestormer/test.py",
+        "args": ["-opt", "options/test/001_xrestormer_sr.yml"],
+        "cwd": BASE_DIR / "X-Restormer",
+    },
 ]
 
 for proj in projects:
     print(f"Running {proj['script']}...")
     subprocess.run(
         [str(proj["venv"]), str(proj["script"]), *proj["args"]],
-        cwd=proj["cwd"],  # set working dir to DarkIR
+        cwd=proj["cwd"],
         check=True,
     )
-
